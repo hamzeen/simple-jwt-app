00aedbd30816ea3c9bbe4f8fb5d5ecf2e95d3557
Mohamed Hameem <lupmyt1@fvfww2gyhv2h.us.aegon.com>
2019-03-13 15:16:22 +0100

    first commit

---
+++ src/server.js
@@ -0,0 +1,69 @@
+const { join } = require('path');
+const pug = require('pug');
+const jwt = require('jsonwebtoken');
+const pointOfView = require('point-of-view');
+const fastifyStatic = require('fastify-static');
+const fastifyFormBody = require('fastify-formbody');
+
+const expectedUsername = process.env.USERNAME || 'albert';
+const expectedPassword = process.env.PASSWORD || 'einstein';
+const jwtSecret = process.env.SECRET || 'secret';
+
+module.exports = (fastify, opts, next) => {
+  fastify.register(fastifyFormBody);
+
+  fastify.register(pointOfView, {
+    engine: {pug},
+    templates: join(__dirname, 'templates'),
+    options: {
+      pretty: true,
+      filename: 'Pug',
+      basedir: join(__dirname, 'templates')
+    }
+  });
+
+  fastify.register(fastifyStatic, {
+    root: join(__dirname, 'assets'),
+    prefix: '/assets/'
+  });
+
+  fastify.get('/', (req, reply) => {
+    const sid = req.query.sid;
+
+    let error = false;
+    let session = null;
+
+    if (sid) {
+      try {
+        session = jwt.verify(sid, jwtSecret)
+      } catch (err) {
+        error = 'Invalid session token'
+      }
+    }
+
+    reply.view('index.pug', {session, error})
+  });
+
+  fastify.get('/login', (req, reply) => {
+    console.log(req.query)
+    reply.view('login.pug', {hasError: req.query.error})
+  });
+
+  fastify.post('/login', (req, reply) => {
+    const {username, password} = req.body;
+    if (
+      username !== expectedUsername ||
+      password !== expectedPassword
+    ) {
+      return reply.redirect('/login?error=1')
+    }
+
+    // create jwt token
+    const jwtToken = jwt.sign({ sub: username, role: 'user' }, jwtSecret)
+
+    // redirect with parameter
+    return reply.redirect(`/?sid=${jwtToken}`)
+  });
+
+  next();
+};
